7dc8826f4d28434345bc442bb460d6ca224e57c6
Jesse Rogers <jesserileyrogers@gmail.com>
2021-06-03 12:00:47 -0400

    request specs and starting the DRY section

---
+++ spec/requests/home_spec.rb
@@ -0,0 +1,7 @@
+describe 'Home Page', type: :request do
+    it "responds successfully" do
+        get root_path
+        expect(response).to be_success
+        expect(response).to have_http_status("200")
+    end
+end
